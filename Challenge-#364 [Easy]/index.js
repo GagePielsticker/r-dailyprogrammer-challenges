f08000b0cd8027d25fae0a43e75ceb438eef94bd
Gage Pielsticker <gagepays@gmail.com>
2022-03-30 14:15:56 -0500

    Submission for challenge #364

---
+++ Challenge-#364 [Easy]/index.js
@@ -0,0 +1,15 @@
+/* User Input */
+let input = '3d6'
+
+/* Split it based on 'd' into an array */
+let inputArr = input.split('d')
+let output = 0
+
+/* For each dice, roll random in range, output and add answer to final output */
+for(let i = 0; i < inputArr[0]; i++) {
+    let ans = Math.floor(Math.random() * inputArr[1] + 1)
+    console.log(`Dice ${i + 1}: ${ans}`)
+    output += ans
+}
+
+console.log(output)
